b74c3157fac78caec89adef69aa01d706ad67fce
Juyoung Suk <juyoung.suk@trillionlabs.co>
2025-09-12 13:58:38 +0900

    Add proto compiled files

---
+++ android_env/proto/a11y/android_accessibility_tree_pb2.pyi
@@ -0,0 +1,14 @@
+from android_env.proto.a11y import android_accessibility_node_info_pb2 as _android_accessibility_node_info_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class AndroidAccessibilityTree(_message.Message):
+    __slots__ = ("nodes",)
+    NODES_FIELD_NUMBER: _ClassVar[int]
+    nodes: _containers.RepeatedCompositeFieldContainer[_android_accessibility_node_info_pb2.AndroidAccessibilityNodeInfo]
+    def __init__(self, nodes: _Optional[_Iterable[_Union[_android_accessibility_node_info_pb2.AndroidAccessibilityNodeInfo, _Mapping]]] = ...) -> None: ...
